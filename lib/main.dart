e66668bbf0692f11a3b5fbb4dbdf59d66179bdc7
Ahmed Saleh <>
2021-03-06 23:33:40 +0200

    add name and job title

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 void main() => runApp(BusinessCard());
 
 class BusinessCard extends StatelessWidget {
+  final String _name = 'Ahmed Saleh';
+  final String _jobTitle = 'Engineer';
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -10,6 +13,7 @@ class BusinessCard extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.teal,
         scaffoldBackgroundColor: Colors.teal,
+        fontFamily: 'Source Sans Pro',
       ),
       home: Scaffold(
         body: SafeArea(
@@ -19,6 +23,24 @@ class BusinessCard extends StatelessWidget {
               CircleAvatar(
                 radius: 50,
                 backgroundImage: AssetImage('images/face.png'),
+              ),
+              Text(
+                _name,
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              Text(
+                _jobTitle.toUpperCase(),
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.teal.shade100,
+                  letterSpacing: 2.5,
+                ),
               ),
             ],
           ),
